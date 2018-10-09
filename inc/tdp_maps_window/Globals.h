89a52bfce9465322c7522828c1090467f9868399
Tom Paynter <tompaynter@tdpe.co.uk>
2018-10-09 13:59:01 +0100

    Merged from private repos.

--- inc/tdp_maps_window/Globals.h
+++ inc/tdp_maps_window/Globals.h
@@ -9,10 +9,17 @@
 #  define TDP_MAPS_WINDOW_SHARED_EXPORT
 #endif
 
+#include "tp_maps/textures/BasicTexture.h"
+
+class QImage;
+
 //##################################################################################################
 //! A simple 3D engine for window based applications
 namespace tdp_maps_window
 {
+
+//##################################################################################################
+tp_maps::TextureData convertTexture(const QImage& image);
 
 }
 
--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,6 +1,36 @@
 #include "tdp_maps_window/Globals.h"
 
+#include <QImage>
+
 namespace tdp_maps_window
 {
+#warning move this somewhere common
+//##################################################################################################
+tp_maps::TextureData convertTexture(const QImage& image)
+{
+  tp_maps::TextureData textureData;
+
+  QImage img = image.mirrored(false, true);
+  img = img.convertToFormat(QImage::Format_ARGB32);
+
+  textureData.w = img.width();
+  textureData.h = img.height();
+
+  textureData.data = new tp_maps::Pixel[size_t(textureData.w * textureData.h)];
+  {
+    const uchar* p = img.bits();
+    const uchar* pMax = p + (img.bytesPerLine() * img.height());
+    tp_maps::Pixel* dst = textureData.data;
+    for(; p<pMax; p+=4, dst++)
+    {
+      dst->r = p[2];
+      dst->g = p[1];
+      dst->b = p[0];
+      dst->a = p[3];
+    }
+  }
+
+  return textureData;
+}
 
 }
