47dfed5fb6160773a0bf7b55f0d3c8896f4f226b
Tom Paynter <tompaynter@tdpe.co.uk>
2018-10-10 11:55:19 +0100

    Started work on ArKit integration.

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -4,8 +4,10 @@
 
 namespace tdp_maps_window
 {
-#warning move this somewhere common
+
 //##################################################################################################
+//Warning: This method is duplicated in both tdp_maps_window and tdp_maps_widget, please make
+//changes to both.
 tp_maps::TextureData convertTexture(const QImage& image)
 {
   tp_maps::TextureData textureData;
